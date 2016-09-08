3a48918667ecfa7b1b6ebd6ff2dd0da107a7fd8d
ZehuaZhang <devorj@hotmail.com>
2016-09-08 14:51:24 -0700

    leetcode C++

--- algorithm/kth-smallest-element-in-a-sorted-matrix.cpp
+++ algorithm/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,4 +1,24 @@
-// Time:  O(k * log(min(n, m, k))), with n x m matrix
+378. Kth Smallest Element in a Sorted Matrix
+Difficulty: Medium
+
+Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
+
+Note that it is the kth smallest element in the sorted order, not the kth distinct element.
+
+Example:
+
+matrix = [
+   [ 1,  5,  9],
+   [10, 11, 13],
+   [12, 13, 15]
+],
+k = 8,
+
+return 13.
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ n2.
+
+// Time:  O(k * log(min(n, m, k))), with m x n matrix
 // Space: O(min(n, m, k))
 
 class Solution {
@@ -9,14 +29,8 @@ public:
         using P = pair<int, pair<int, int>>;
         priority_queue<P, vector<P>, greater<P>> q;
         auto push = [&matrix, &q](int i, int j) {
-            if (matrix.size() > matrix[0].size()) {
-                if (i < matrix[0].size() && j < matrix.size()) {
-                    q.emplace(matrix[j][i], make_pair(i, j));
-                }
-            } else {
-                if (i < matrix.size() && j < matrix[0].size()) {
-                    q.emplace(matrix[i][j], make_pair(i, j));
-                }
+            if (i < matrix.size() && j < matrix[0].size()) {
+                q.emplace(matrix[i][j], make_pair(i, j));
             }
         };
 
