2caddd10347d429f1d2148ae428eda6b10061606
ZehuaZhang <devorj@hotmail.com>
2017-02-18 02:07:47 -0800

    singletonOC

--- algorithm/templates/singletonOC.m
+++ algorithm/templates/singletonOC.m
@@ -2,7 +2,6 @@
 
 @interface Singleton : NSObject
 
-- (id) init;
 + (Singleton*) instance;
 + (float) frameRate;
 + (void) setFrameRate:(float)fr;
@@ -11,22 +10,26 @@
 
 @implementation Singleton
 
-static Singleton* _gInstance = NULL;
+static Singleton* _gInstance = nil;
 float _ivarFrameRate;
+
+int _ha;
 
 + (Singleton*)instance
 {
   @synchronized(self)
   {
     if (_gInstance == NULL)
-      _gInstance = [[self alloc] init];
+      _gInstance = [[super allocWithZone:NULL] init];
   }
   return _gInstance;
 }
 
-- (id)init
-{
-  self = [super init];
++ (id) allocWithZone:(NSZone *)zone {
+  return [self instance];
+}
+
+- (id) copyWithZone:(NSZone *)zone {
   return self;
 }
 
@@ -49,13 +52,3 @@ float _ivarFrameRate;
 }
 
 @end
-
-int main() {
-  @autoreleasepool {
-    NSLog(@"Singleton Framerate = %f", Singleton.frameRate);
-    NSLog(@"Singleton Framerate = %f", Singleton.frameRate = 8.0);
-    NSArray* hereIGo = @[@"Zehua Zhang"];
-    NSArray* zehua = [NSArray arrayWithArray:hereIGo];
-    NSLog(@"%@%@", zehua, @"Facebook!");
-  }
-}
