6505fe4ebce790b7431a6c30573b012cdf506a67
ZehuaZhang <devorj@hotmail.com>
2017-04-07 15:23:49 -0700

    cpp updated

--- algorithm/_misc/find-predecessor-successor-in-bst.cpp
+++ algorithm/_misc/find-predecessor-successor-in-bst.cpp
@@ -2,54 +2,59 @@
 
 // findPredSucc(root, target, nullptr, nullptr);
 
-void findPredSucc(TreeNode *root, int target, TreeNode* &pred, TreeNode* &succ) {
-	while (root) {
-		if (root->val == target) {
-			if (root->left) {
-				pred = root->left;
-				while (pred->right) {
-					pred = pred->right;
-				}
-			}
-			if (root->right) {
-				succ = root->right;
-				while (succ->left) {
-					succ = succ->left;
-				}
-			}
-		} else if (root->val > target) {
-			succ = root;
-			root = root->left;
-		} else {
-			pred = root;
-      root = root->right;
+class Solution {
+public:
+  void findPredSucc(TreeNode* root, int target, TreeNode*& pred, TreeNode*& succ) {
+    while (root) {
+      if (root->val == target) {
+        if (root->left) {
+          pred = root->left;
+          while (pred->right) {
+            pred = pred->right;
+          }
+        }
+        if (root->right) {
+          succ = root->right;
+          while (succ->left) {
+            succ = succ->left;
+          }
+        }
+      } else if (root->val > target) {
+        succ = root;
+        root = root->left;
+      } else {
+        pred = root;
+        root = root->right;
+      }
     }
   }
-}
+};
 
 // with parent node
-
-void findPredSucc(TreeNode* root, TreeNode* node, TreeNode* &pred, TreeNode* &succ) {
-  if (node) {
-    if (node->left) {
-      pred = node->left;
-      while (pred->right) {
-        pred = pred->right;
+class Solution {
+public:
+  void findPredSucc(TreeNode* root, TreeNode* node, TreeNode* &pred, TreeNode* &succ) {
+    if (node) {
+      if (node->left) {
+        pred = node->left;
+        while (pred->right) {
+          pred = pred->right;
+        }
+      }
+      if (node->right) {
+        succ = node->right;
+        while (succ->left) {
+          succ = succ->left;
+        }
       }
     }
-    if (node->right) {
-      succ = node->right;
-      while (succ->left) {
-        succ = succ->left;
-      }
+    if (!node->right) {
+      succ = node->parent;
+      for (TreeNode* curr = node; succ && succ->right == curr; curr = succ, succ = succ-> parent);
+    }
+    if (!node->left) {
+      prev = node->parent;
+      for (TreeNode* curr = node; prev && prev->left == curr; curr = prev, prev = prev-> parent);
     }
   }
-  if (!node->right) {
-    succ = node->parent;
-    for (TreeNode* curr = node; succ && succ->right == curr; curr = succ, succ = succ-> parent);
-  }
-  if (!node->left) {
-    prev = node->parent;
-    for (TreeNode* curr = node; prev && prev->left == curr; curr = prev, prev = prev-> parent);
-  }
-}
+};
--- algorithm/_misc/merge-k-sorted-arrays.cpp
+++ algorithm/_misc/merge-k-sorted-arrays.cpp
@@ -1,24 +1,27 @@
 // Merge K Sorted Arrays
 
-vector<int> mergeKArrays(vector<vector<int>>sorted) {
-  vector<int> result;
-  pririty_queue<int, vector<int>, less<int>> minHeap;
+class Solution {
+public:
+  vector<int> mergeKArrays(vector<vector<int>>sorted) {
+    vector<int> result;
+    pririty_queue<int, vector<int>, greater<int>> minHeap;
 
-  for (int i = 0; i < sorted.size(); i++) {
-    if (!sorted[i].empty()) {
-      minHeap.push(make_tuple(sorted[i][0], i, 0));
+    for (int i = 0; i < sorted.size(); i++) {
+      if (!sorted[i].empty()) {
+        minHeap.push(make_tuple(sorted[i][0], i, 0));
+      }
     }
-  }
-  
-  while (!minHeap.empty()) {
-    int e, i, j;
-    tie(e, i , j) = minHeap.top(); minHeap.pop();
 
-    result.push_back(e);
-    
-    if (j + 1 < sorted[i].size()) {
-      minHeap.push(make_tuple(sorted[i][j + 1], i, j + 1));
+    while (!minHeap.empty()) {
+      int e, i, j;
+      tie(e, i, j) = minHeap.top(); minHeap.pop();
+
+      result.push_back(e);
+
+      if (j + 1 < sorted[i].size()) {
+        minHeap.push(make_tuple(sorted[i][j + 1], i, j + 1));
+      }
     }
+    return result;
   }
-  return result;
-}
+};
