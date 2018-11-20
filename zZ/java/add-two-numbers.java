5ae73de7420b1d461383614cfc36e6a1f223ea3c
Zehua Zhang <zezh@microsoft.com>
2018-11-20 15:22:16 -0800

    add one more problem

--- zZ/java/add-two-numbers.java
+++ zZ/java/add-two-numbers.java
@@ -15,6 +15,7 @@ public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         int carry = 0;
         ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
         while (l1 != null || l2 != null || carry) {
             int num1 = l1 == null ? 0 : l1.val;
             int num2 = l2 == null ? 0 : l2.val;
