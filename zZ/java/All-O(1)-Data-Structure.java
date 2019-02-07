4afac3c86761c9b93dc041ab0078c21db8cb8978
zehua zhang <devorj@hotmail.com>
2019-02-07 13:53:26 -0800

    updated to design 33

--- zZ/java/All-O(1)-Data-Structure.java
+++ zZ/java/All-O(1)-Data-Structure.java
@@ -11,19 +11,115 @@
  * Challenge: Perform all these in O(1) time complexity.
  */
 
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class AllOne  {
     AllOne () {
+        head = new Bucket(Integer.MIN_VALUE);
+        tail = new Bucket(Integer.MAX_VALUE);
+        keyBucketMap = new HashMap<>();
+
+        head.next = tail;
+        tail.prev = head;
     }
 
     public void inc(String key) {
+        if (keyBucketMap.containsKey(key)) {
+            change(key, 1);
+        } else {
+            if (head.next.count != 1) {
+                Bucket bucket = new Bucket(1);
+                addNewBucketAfter(head, bucket);
+            }
+            keyBucketMap.put(key, head.next);
+            head.next.keySet.add(key);            
+        }
     }
     
     public void dec(String key) {
+        if (keyBucketMap.containsKey(key)) {
+            Bucket bucket = keyBucketMap.get(key);
+            if (bucket.count != 1) {
+                change(key, -1);
+            } else {
+                removeKeyFromBucket(key, bucket);
+                keyBucketMap.remove(key);
+            }
+        }
     }
     
     public String getMaxKey() {
+        if (tail.prev == head) {
+            return "";
+        }
+
+        return tail.prev.keySet.iterator().next();
     }
     
     public String getMinKey() {
+        if (head.next == tail) {
+            return "";
+        }
+
+        return head.next.keySet.iterator().next();
+    }
+
+    private change(String key, int offset) {
+        Bucket bucket = keyBucketMap.get(key);
+        int count = bucket.count;
+        int newCount = count + offset;
+
+        Bucket nextBucket = offset > 0 ? bucket.next : bucket.prev;
+        if (nextBucket.count != newCount) {
+            nextBucket = new Bucket(newCount);
+            addNewBucketAfter(offset > 0 ? bucket : bucket.prev, nextBucket);
+        }
+
+        keyBucketMap.put(key, nextBucket);
+        nextBucket.keySet.add(key);
+        removeKeyFromBucket(key, bucket);
+    }
+
+    private void removeKeyFromBucket(String key, Bucket bucket) {
+        bucket.keySet.remove(key);
+        if (bucket.keySet.isEmpty()) {
+            removeBucketFromList(bucket);
+        }
+    }
+
+    private void removeBucketFromList(Bucket bucket) {
+        bucket.next.prev = bucket.prev;
+        bucket.prev.next = bucket.next;
+        bucket.next = null;
+        bucket.prev = null;
+    }
+
+    private void addNewBucketAfter(Bucket bucket, Bucket newBucket) {
+        newBucket.next = bucket.next;
+        newBucket.prev = bucket;
+        bucket.next.prev = newBucket;
+        bucket.next = newBucket;
+    }
+
+    private Bucket head;
+    private Bucket tail;
+    private Map<String, Bucket> keyBucketMap;
+
+    public class Bucket {
+        int count;
+        Set<String> keySet;
+
+        Bucket prev;
+        Bucket next;
+
+        Bucket(int count) {
+            this.count = count;
+            this.keySet = new HashSet<>();
+
+            prev = next = null;
+        }
     }
 }
--- zZ/java/lfu-cache.java
+++ zZ/java/lfu-cache.java
@@ -25,13 +25,107 @@
  * cache.get(4);       // returns 4
  */
 
-public class LFUCache   {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LFUCache {
     LFUCache (int capacity) {
+        keyBucketMap = new HashMap<>();
+        keyValueMap = new HashMap<>();
+        head = new Bucket(Integer.MIN_VALUE);
+        tail = new Bucket(Integer.MAX_VALUE);
+        this.capacity = capacity;
+
+        head.next = tail;
+        tail.prev = head;
     }
 
     public int get(int key) {
+        if (keyBucketMap.containsKey(key)) {
+            change(key);
+            return keyValueMap.get(key);
+        }
+
+        return -1;
     }
     
     public void put(int key, int value) {
+        if (!keyBucketMap.containsKey(key)) {
+            if (keyBucketMap.size == capacity) {
+                int removeKey = head.next.keySet.iterator().next();
+                removeKeyFromBucket(removeKey, head.next);
+                keyBucketMap.remove(removeKey);
+            }
+
+            if (head.next.count != 1) {
+                addNewBucketAfter(head, new Bucket(1));
+            }
+            keyBucketMap.put(key, head.next);
+            head.next.keySet.add(key);
+        } else {
+            change(key);
+        }
+
+        keyValueMap.put(key, value);
+    }
+
+    private void change(int key) {
+        Bucket bucket = keyBucketMap.get(key);
+        int count = bucket.count;
+        int newCount = count + 1;
+
+        Bucket nextBucket = bucket.next;
+        if (nextBucket.count != newCount) {
+            nextBucket = new Bucket(count);
+            addNewBucketAfter(bucket, newBucket);
+        }
+
+        keyBucketMap.put(key, nextBucket);
+        nextBucket.keySet.add(key);
+        removeKeyFromBucket(key, bucket);
+    }
+
+    private void removeKeyFromBucket(int key, Bucket bucket) {
+        bucket.keySet.remove(key);
+        if (bucket.keySet.isEmpty()) {
+            removeBucketFromList(bucket);
+        }
+    }
+
+    private void removeBucketFromList(Bucket bucket) {
+        bucket.next.prev = bucket.prev;
+        bucket.prev.next = bucket.next;
+        bucket.prev = null;
+        bucket.next = null;
+    }
+
+    private void addNewBucketAfter(Bucket bucket, Bucket newBucket) {
+        newBucket.next = bucket.next;
+        newBucket.prev = bucket;
+        bucket.next.prev = newBucket;
+        bucket.next = newBucket;
+    }
+
+    private Bucket head;
+    private Bucket tail;
+    private Map<Integer, Bucket> keyBucketMap;
+    private Map<Integer, Integer> keyValueMap;
+    private int capacity;
+
+    private class Bucket {
+        int count;
+        Set<Integer> keySet;
+        Bucket prev;
+        Bucket next;
+
+        Bucket(int count) {
+            this.count = count;
+            keySet = new HashSet<>();
+
+            prev = null;
+            next = null;
+        }
     }
 }
