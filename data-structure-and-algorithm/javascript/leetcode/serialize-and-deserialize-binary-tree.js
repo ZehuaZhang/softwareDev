ff1e69526a720fc1188034fb24ded199c3170974
o D <zehua.zhang@zumepizza.com>
2021-06-26 15:13:06 -0700

    more leetcode

--- data-structure-and-algorithm/javascript/leetcode/serialize-and-deserialize-binary-tree.js
+++ data-structure-and-algorithm/javascript/leetcode/serialize-and-deserialize-binary-tree.js
@@ -1,47 +1,67 @@
-/**
- * Encodes a tree to a single string.
- *
- * @param {TreeNode} root
- * @return {string}
- */
-function serialize(root) {
-    const result = []
-    serializeHelper(root, result)
-    
-    return result.join(" ")
-}
+/*
+Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
 
-function serializeHelper(root, result) {
-    if (!root) {
-        result.push("#")
+Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+Clarification: The input/output format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,null,null,4,5]
+Output: [1,2,3,null,null,4,5]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+Input: root = [1,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 104].
+-1000 <= Node.val <= 1000
+*/
+
+var serialize = function(root) {
+    const result = [];
+    serializeDFS(root, result);
+    return result.join(" ");
+};
+
+function serializeDFS(node, result) {
+    if (node === null) {
+        result.push("#");
     } else {
-        result.push(root.val)
-        serializeHelper(root.left, result)
-        serializeHelper(root.right, result)
+        result.push(node.val);
+        serializeDFS(node.left, result);
+        serializeDFS(node.right, result);
     }
 }
 
-/**
- * Decodes your encoded data to tree.
- *
- * @param {string} data
- * @return {TreeNode}
- */
-function deserialize(data) {
-    data = data.split(" ")
-    const iterator = data[Symbol.iterator]()
-    return deserializeHelper(iterator)
-}
+var deserialize = function(data) {
+    list = data.split(" ");
+    const iter = list.values();
+    return deserializeDFS(iter);
+};
 
-function deserializeHelper(iterator) {
-    const value = iterator.next().value
-   if (value === "#") {
-        return null
-    } 
+function deserializeDFS(iter) {
+    const {value, done} = iter.next();
+    if (done || value === "#" ) {
+        return null;
+    }
     
-    const node = new TreeNode(value)
-    node.left = deserializeHelper(iterator)
-    node.right = deserializeHelper(iterator)
-
-    return node
+    const node = new TreeNode(value);
+    node.left = deserializeDFS(iter);
+    node.right = deserializeDFS(iter);
+    return node;
 }
--- data-structure-and-algorithm/javascript/leetcode/word-search-ii.js
+++ data-structure-and-algorithm/javascript/leetcode/word-search-ii.js
@@ -36,6 +36,51 @@ class TrieNode {
     }
 }
 
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(s) {
+        let curr = this.root;
+        for (const c of s) {
+            if (!curr.leaves.has(c)) {
+                curr.leaves.set(c, new TrieNode());
+            }
+            curr = curr.leaves.get(c);
+        }
+        curr.string = s;
+    }
+}
+
 function findWords(board, words) {
-    
+    const t = new Trie();
+    const result = [];
+    const visited = Array(board.length).fill(0).map(() => Array(board[0].length).fill(false));
+    for (const word of words) {
+        t.insert(word);
+    }
+    for (let i = 0; i < board.length; ++i) {
+        for (let j = 0; j < board[0].length; ++j) {
+            if (t.root.leaves.has(board[i][j])) {
+                findWordsDFS(board, t.root.leaves.get(board[i][j]), i, j, visited, result);
+            }
+        }
+    }
+    return result;
+}
+
+function findWordsDFS(board, node, i, j, visited, result) {
+    if (node.string.length !== 0) {
+        result.push(node.string);
+        node.string = "";
+    }
+    visited[i][j] = true;
+    for (const [dx, dy] of [[-1, 0], [1, 0], [0, -1], [0, 1]]) {
+        const x = i + dx, y = j + dy;
+        if (x >= 0 && x < board.length && y >= 0 && y < board[0].length && node.leaves.has(board[x][y]) && !visited[x][y]) {
+            findWordsDFS(board, node.leaves.get(board[x][y]), x, y, visited, result);
+        }
+    }
+    visited[i][j] = false;
 }
