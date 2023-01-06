90f95c6988defbd5ce0aff54572b970ebd5a6d03
oD <oodaniel00@oDs-Mac-Pro.askey.com>
2023-01-06 12:56:42 -0800

    add bigint support

--- data-structure-and-algorithm/typescript/leetcode/src/extra-questions/publish-subscribe.ts
+++ data-structure-and-algorithm/typescript/leetcode/src/extra-questions/publish-subscribe.ts
@@ -1,52 +1,75 @@
-class Subscriber {
-  subId: string;
-  handle: Function;
-  constructor(subId: string, handle: Function) {
-    this.subId = subId;
-    this.handle = handle;
-  }
-}
-
-class PublishSubscribe<T> {
-  subId: number;
-  eventSubListMap: Map<string, Subscriber[]>;
+class PublishSubscribe {
+  subId: bigint;
+  eventSubIdSetMap: Map<string, Set<string>>;
+  subIdSubscriberMap: Map<string, Subscriber>;
 
   constructor() {
-    this.eventSubListMap = new Map<string, Subscriber[]>();
-    this.subId = 0;
+    this.eventSubIdSetMap = new Map<string, Set<string>>();
+    this.subIdSubscriberMap = new Map<string, Subscriber>();
+    this.subId = 0n;
   }
 
-  publish(event: string, data: T): boolean {
-    if (!this.eventSubListMap.has(event)) {
+  emit(event: string, ...args: any[]): boolean {
+    if (!this.eventSubIdSetMap.has(event)) {
       return false;
     }
 
-    const subList = this.eventSubListMap.get(event)!;
-    subList.forEach(sub => {
-      sub.handle(event, data);
+    const subIdList = this.eventSubIdSetMap.get(event)!;
+    subIdList.forEach(subId => {
+      const subscriber = this.subIdSubscriberMap.get(subId)!;
+      subscriber.handle(...args);
     });
     return true;
   }
 
-  subscribe(event: string, handle: Function): string {
-    if (!this.eventSubListMap.has(event)) {
-      this.eventSubListMap.set(event, []);
+  subscribe(event: string, handle: Function): Subscriber {
+    if (!this.eventSubIdSetMap.has(event)) {
+      this.eventSubIdSetMap.set(event, new Set<string>());
     }
 
     const subId = (++this.subId).toString();
-    this.eventSubListMap.get(event)!.push(new Subscriber(subId, handle));
-    return subId;
+    const subIdSet = this.eventSubIdSetMap.get(event)!;
+    subIdSet.add(subId);
+    const subscriber = new Subscriber(subId, handle, () => {
+      if (!subIdSet.has(subId)) {
+        return false;
+      }
+      subIdSet.delete(subId);
+      this.subIdSubscriberMap.delete(subId);
+      if (subIdSet.size === 0) {
+        this.eventSubIdSetMap.delete(event);
+      }
+      return true;
+    });
+    this.subIdSubscriberMap.set(subId, subscriber);
+    return subscriber;
   }
 
-  unsubscribe(subId: string): boolean {
-    for (const subList of this.eventSubListMap.values()) {
-      for (let i = 0; i < subList.length; ++i) {
-        if (subId === subList[i].subId) {
-          subList.splice(i, 1);
-          return true;
+  unsubscribe(subId: string) {
+    if (!this.subIdSubscriberMap.has(subId)) {
+      return false;
+    }
+    for (const [event, subIdSet] of this.eventSubIdSetMap.entries()) {
+      if (subIdSet.has(subId)) {
+        subIdSet.delete(subId);
+        this.subIdSubscriberMap.delete(subId);
+        if (subIdSet.size === 0) {
+          this.eventSubIdSetMap.delete(event);
         }
+        return true;
       }
     }
     return false;
+  }
+}
+
+class Subscriber {
+  subId: string;
+  handle: Function;
+  unsubscribe: () => boolean;
+  constructor(subId: string, handle: Function, unsubscribe: () => boolean) {
+    this.subId = subId;
+    this.handle = handle;
+    this.unsubscribe = unsubscribe;
   }
 }
